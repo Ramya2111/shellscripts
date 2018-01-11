b48b9ac4d10c598fac3175f2bf3fd3d7051ac80f
Ramya2111 <33828305+Ramya2111@users.noreply.github.com>
2018-01-11 18:03:24 +0530

    Update test.sh

--- programs/test.sh
+++ programs/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-read -p "Enter file name:" file1
-           if [ -s $file1 ]
+read -p "Enter file name:" file
+           if [ -s $file ]
            then
              echo "File exists"
            else
              echo "Provide contents to the file and type EOF at the end to create file"
-             cat <<'EOF'>> '$file1'
+             cat <<'EOF'>> '$file'
              echo "File created"
            fi
